f845c5aa3a405647a6494a8708155cfe4289fb0f
Самарин Дмитрий <d.samarin@solarl.ru>
2023-03-09 19:43:28 +0300

    Добавил сервисы

---
+++ src/Board/Application/Board.Application.AppData/Contexts/Posts/Services/IPostService.cs
@@ -0,0 +1,17 @@
+﻿using Board.Contracts.Posts;
+
+namespace Board.Application.AppData.Contexts.Posts.Services;
+
+/// <summary>
+/// Сервис для работы с объявлениями.
+/// </summary>
+public interface IPostService
+{
+    /// <summary>
+    /// Создает объявление.
+    /// </summary>
+    /// <param name="dto">Модель создания объявления.</param>
+    /// <param name="cancellation">Токен отмены операции.</param>
+    /// <returns>Модель объявления.</returns>
+    Task<CreatePostDto> AddPost(CreatePostDto dto, CancellationToken cancellation);
+}
---
+++ src/Board/Contracts/Board.Contracts/Posts/CreatePostDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Board.Contracts.Posts;
+
+/// <summary>
+/// Модель создания объявления.
+/// </summary>
+public class CreatePostDto
+{
+    /// <summary>
+    /// Наименование.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Описание.
+    /// </summary>
+    public string Description { get; set; }
+    
+    /// <summary>
+    /// Дата создания.
+    /// </summary>
+    public DateTime CreationDate { get; set; }
+}
