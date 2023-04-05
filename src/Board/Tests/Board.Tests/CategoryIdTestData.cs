00f00c9c3207b0d55ddc68794a2710548e97ab31
Лазаренко Дмитрий <d.lazarenko@solarl.ru>
2023-04-05 21:05:38 +0300

    + параметризованные unit-тесты

---
+++ src/Board/Tests/Board.Tests/CategoryIdTestData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Board.Tests
+{
+    public class CategoryIdTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator() =>
+            CategoryListFixture.Ids.Select(x => new object[] { x }).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
