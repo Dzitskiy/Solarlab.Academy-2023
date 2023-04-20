4dfc117bd04dc700d5524d4447c4ad7a947e44d4
Automerge <automerge@rts-tender.ru>
2023-04-20 16:23:16 +0300

    Add Redis Cache

--- src/Board/Application/Board.Application.AppData/Contexts/Categories/Services/CategoryService.cs
+++ src/Board/Application/Board.Application.AppData/Contexts/Categories/Services/CategoryService.cs
@@ -3,6 +3,10 @@ using Board.Application.AppData.Contexts.Categories.Repositories;
 using Board.Contracts.Category;
 using Board.Domain.Categories;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+//using Newtonsoft.Json;
 
 namespace Board.Application.AppData.Contexts.Categories.Services
 {
@@ -14,12 +18,14 @@ namespace Board.Application.AppData.Contexts.Categories.Services
         private readonly ICategoryRepository _categoryRepository;
         private readonly IMapper _mapper;
         private readonly IMemoryCache _memoryCache;
+        private readonly IDistributedCache _distributedCache;
 
-        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper, IMemoryCache memoryCache)
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper, IMemoryCache memoryCache, IDistributedCache distributedCache)
         {
             _categoryRepository = categoryRepository;
             _mapper = mapper;
             _memoryCache = memoryCache;
+            _distributedCache = distributedCache;
         }
 
         /// <inheritdoc/>
@@ -32,9 +38,16 @@ namespace Board.Application.AppData.Contexts.Categories.Services
         /// <inheritdoc/>
         public async Task<CategoryInfoDto[]> GetActiveAsync(CancellationToken cancellationToken)
         {
+            //memoryCache
             if (_memoryCache.TryGetValue(ActiveCategoriesCachingKey, out CategoryInfoDto[] result))
             {
                 return result;
+            }
+            //distributedCache
+            string serializedString = await _distributedCache.GetStringAsync(ActiveCategoriesCachingKey);
+            if (serializedString != null)
+            {
+                return JsonSerializer.Deserialize<CategoryInfoDto[]>(serializedString);
             }
 
             result = await _categoryRepository.GetActiveAsync(cancellationToken);
@@ -44,6 +57,15 @@ namespace Board.Application.AppData.Contexts.Categories.Services
                 AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30)
             };
             _memoryCache.Set(ActiveCategoriesCachingKey, result, options);
+
+            await _distributedCache.SetStringAsync(
+                                key: ActiveCategoriesCachingKey,
+                                value: JsonSerializer.Serialize(result),
+                                options: new DistributedCacheEntryOptions
+                                {
+                                    SlidingExpiration =  TimeSpan.FromSeconds(30),
+                                    AbsoluteExpiration = DateTimeOffset.UtcNow.AddHours(1)
+                                });
 
             return result;
         }
