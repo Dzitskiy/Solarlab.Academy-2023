03fa18017a5f32edf60614322a181beef552ee23
Automerge <automerge@rts-tender.ru>
2023-04-27 18:05:25 +0300

    Commit

---
+++ RealTimeProj/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTimeProj
+{
+    public class ChatHub : Hub
+    {
+        //public async Task Send(string nickname, string message) 
+        //{
+        //    await Clients.All.SendAsync("Send",nickname, message);  
+        //}
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
